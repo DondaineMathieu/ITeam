5d8e83514f66a46dcf8096f4add212a2fbeeb47d
DondaineMathieu <dondaine.mathieu@gmail.com>
2019-02-26 15:59:18 +0100

    previous and next button

--- client/src/Quizz.js
+++ client/src/Quizz.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {quizzes, users} from './examples';
+import {Link} from 'react-router-dom';
 
 import {HTTP_SERVER_PORT_PICTURES} from './constants.js';
 
@@ -8,15 +9,43 @@ import {HTTP_SERVER_PORT_PICTURES} from './constants.js';
 class Question extends Component {
     constructor(props) {
         super(props);
+        
     }
 
     render() {
+      const c = this.props.current;
+      const q = this.props.questions[c];
+      const bp =  c > 0 ? <input type="button" value="Previous questions" onClick={this.props.previousQuestion} /> : null; 
+      const ba =  c < this.props.questions.length ? <input type="button" value="Next Question" onClick={this.props.nextQuestion} /> : <input type="button" value="See results" onClick="result()" />;
+
       return (
-        <div><h1>{this.props.question.question}</h1>
-        {this.props.question.txtAnswers.map(txt => <div><input type='checkbox' /> {txt}</div>)}
-        {this.props.question.imgAnswers.map(img => <div><input type='checkbox' /> <img src={HTTP_SERVER_PORT_PICTURES + img} /> </div>)}
-        </div>)
+        <form>
+          <div><h1>{q.question}</h1>
+          <h3></h3>
+          {q.txtAnswers.map(txt => <div><input type='checkbox' /> {txt}</div>)}
+          {q.imgAnswers.map(img => <div><input type='checkbox' /> <img src={HTTP_SERVER_PORT_PICTURES + img} /> </div>)}
+
+          {bp}
+          {ba}
+          {/* { 
+
+             if (question.state == null) {
+              <input type="button" value="Validate" onClick="validate()" />      
+            }
+          } */}
+
+          </div>
+        </form>
+      )
     }
+
+    result() {
+    }
+
+    validate() {
+      this.questions.state = "validate";
+    }
+
   }
 
 class Quizz extends Component {
@@ -24,13 +53,24 @@ class Quizz extends Component {
     constructor(props) {
         super(props);
         this.quizz = quizzes.filter(q=> q._uid == this.props.match.params.id )[0];
-        this.state = {current : 1};
+        this.state = {current : 0};
     }
+
+    nextQuestion() {
+      let newIndex = this.state.current+1;
+      this.setState({current:newIndex});
+    }
+
+    previousQuestion() {
+      let newIndex = this.state.current-1;
+      this.setState({current:newIndex});
+    }
+
   render() {
     return (
-     <div> <h1>{this.quizz.name}</h1>
-
-      <Question question={this.quizz.questions[this.state.current]} />
+     <div> 
+       <h1> {this.quizz.name} </h1>
+      <Question current={this.state.current} questions={this.quizz.questions} nextQuestion={() => this.nextQuestion()} previousQuestion={() => this.previousQuestion()} />
       </div>
 
     )
